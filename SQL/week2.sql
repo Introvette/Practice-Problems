cacb9d6eea4ee69f3564c8079e400695e979f7b1
Yvette Rosario <vette.rosario@gmail.com>
2023-01-04 11:00:13 -0500

    no 4, 5

--- SQL/week2.sql
+++ SQL/week2.sql
@@ -2,3 +2,19 @@ SELECT id, name, website
 from trucks
 WHERE vegetarian_friendly = TRUE
 ORDER BY name
+
+----------------------------------------------------------------
+
+SELECT u.email, t.name
+FROM users u
+INNER JOIN trucks t ON (u.id = t.owner_id)
+ORDER BY t.name
+
+----------------------------------------------------------------
+
+SELECT t.name, COUNT(r.*) AS num_reviews
+FROM trucks AS t
+LEFT OUTER JOIN reviews AS r ON (r.truck_id = t.id)
+GROUP BY t.name
+
+----------------------------------------------------------------
