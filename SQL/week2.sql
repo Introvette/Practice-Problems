d79b9bf1a6609617d8afaaf3bdac49f2c70733bb
Yvette Rosario <vette.rosario@gmail.com>
2023-01-04 11:24:05 -0500

    no. 7, 8, 9

--- SQL/week2.sql
+++ SQL/week2.sql
@@ -14,7 +14,34 @@ ORDER BY t.name
 
 SELECT t.name, COUNT(r.*) AS num_reviews
 FROM trucks AS t
+-- FROM = left table
 LEFT OUTER JOIN reviews AS r ON (r.truck_id = t.id)
 GROUP BY t.name
 
 ----------------------------------------------------------------
+
+SELECT t.name
+, COUNT(r.*) AS num_reviews
+, AVG (r.rating) AS average_rating
+FROM trucks AS t
+LEFT OUTER JOIN reviews AS r on (r.truck_id = t.id)
+GROUP BY t.name
+
+----------------------------------------------------------------
+
+SELECT t.name
+    , COUNT (tmi.*) AS num_items
+    , ROUND ( AVG (tmi.price), 2)
+FROM trucks as t
+LEFT JOIN truck_menu_items AS  tmi
+    ON (t.id = tmi.truck_id)
+GROUP BY t.name
+ORDER BY t.name DESC;
+
+----------------------------------------------------------------
+
+SELECT trucks.name, SUM(menu_items.calories) AS total_calories
+FROM ((trucks
+LEFT OUTER JOIN truck_menu_items ON trucks.id=truck_menu_items.truck_id)
+LEFT OUTER JOIN menu_items ON truck_menu_items.menu_item_id=menu_items.id)
+GROUP BY trucks.name
