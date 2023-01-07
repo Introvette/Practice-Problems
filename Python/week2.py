8aad2d6b63369d628a88f1481b921162e00120d5
Yvette Rosario <vette.rosario@gmail.com>
2023-01-07 14:58:59 -0500

    leetcode

--- Python/week2.py
+++ Python/week2.py
@@ -19,3 +19,21 @@ def parse_calculation_ext(s):
 def parse_calculation(s):
     v = s.strip("()").split()
     return eval(v[1] + v[0] + v[2])
+
+
+# ----------------------------------------------------------------
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if headA == None and headB == None:
+            return None
+        elif headA == None and headB != None:
+            return None
+        elif headA != None and headB == None:
+            return None
+        else:
+            len_a = 0
